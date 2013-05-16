c0d00de041ffd65848e433537fd11367fb8a96e0
Moses Ngone <mo@mospired.com>
2013-05-16 17:56:44 -0400

    moved directory root
    Signed-off-by: Moses Ngone <mo@mospired.com>

---
+++ templates/home.php
@@ -0,0 +1,16 @@
+<?php include "sections/header.php"; ?>
+<div class="container-fluid">
+      <div class="masthead">
+        <h3 class="muted">Blueridge</h3>
+      </div>
+      <hr>
+
+      <div class="jumbotron">
+        <h1>Super awesome marketing speak!</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <a class="btn btn-large btn-success" href="/auth/">Connect With Basecamp</a>
+      </div>
+
+      <hr>
+</div>
+<?php include "sections/footer.php"; ?>
---
+++ templates/sections/footer.php
@@ -0,0 +1,14 @@
+<div id="push"></div>
+  </div>
+  <div id="footer">
+    <div class="container">
+      <p class="muted credit">BlueRidge App: v0.1</p>
+    </div>
+  </div>
+  <script src="/lib/angular/angular.min.js"></script>
+  <script src="/lib/angular/angular-resource.min.js"></script>
+  <script src="/js/app.js"></script>
+  <script src="/js/services.js"></script>
+  <script src="/js/controllers.js"></script>
+</body>
+</html>
