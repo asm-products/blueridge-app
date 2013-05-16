5c41f792de34fcaf889c84cdbb67593fe1401b9a
Moses Ngone <mo@mospired.com>
2013-05-16 16:51:32 -0400

    changed the index view

--- app/index.php
+++ app/index.php
@@ -10,33 +10,58 @@ use \Slim\Slim;
 
 $app = new Slim();
 
-$app->get('/',function() use ($app){
+
+$app->get('/auth/',function() use ($app){
 	/**
 	 * @todo check for session
 	 */
 	// redirect them to basecamp
-	 $app->redirect('https://launchpad.37signals.com/authorization/new?type=web_server&client_id=e391c424f7787e13c608bda67a22c2b121e50418&redirect_uri=http://www.blueridgeapp.com/auth');
+
+	//https://launchpad.37signals.com/authorization/new?type=web_server&client_id=e391c424f7787e13c608bda67a22c2b121e50418&redirect_uri=http://blueridgeapp.com/auth
+
+	$app->redirect('https://launchpad.37signals.com/authorization/new?type=web_server&client_id=e391c424f7787e13c608bda67a22c2b121e50418&redirect_uri=http://blueridgeapp.com/basecamp/');
 });
 
-$app->get('/auth',function() use ($app){
+$app->get('/basecamp/',function() use ($app){
 
-	echo "booya";
-});
-/*
+	$code = $app->request()->get('code');
+	if(!empty($code)){
+		$url = "https://launchpad.37signals.com/authorization/token";
+		$params = [
+		'type'=>'web_server',
+		'client_id'=>'e391c424f7787e13c608bda67a22c2b121e50418',
+		'redirect_uri'=>'http://blueridgeapp.com/auth',
+		'client_secret'=>'c0d00cc97adda78505784d56c3f2eae158db63aa',
+		'code'=>$code
+		];
+		$ch = curl_init($url);
 
-$app->get('/:resource/(:id/)', function ($resource,$id = null) use ($app) {
 
-	$entityName  = "\\BlueRidge\\Entities\\{$app->resource->entity}";
+		$params = http_build_query($params);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $params );
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
+		curl_setopt($ch, CURLOPT_HEADER,0);  
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER ,1); 
 
-	$user= new $entityName;
-	$user->init($app);
-	if (!empty($id)){
+		// grab URL and pass it to the browser
+		$data = curl_exec($ch);
 
-	}else{
-		$collection = $user->fetch();
-		$resourse = json_encode($collection);
+		if(!$data){
+			echo 'no work';
+			var_dump(curl_getinfo($ch));
+			var_dump(curl_error($ch));
+		}else{
+			var_dump($data);
+		}
+
+		// close cURL resource, and free up system resources
+		curl_close($ch);
+
 	}
-	echo $resourse;
 });
-*/
+
+
+
+$app->get('/app/');
 $app->run();
