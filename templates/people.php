26c01320850f077e19c62b542f8c48f2de5ef654
Moses Ngone <mo@mospired.com>
2013-05-19 11:33:32 -0400

    removing angular moving to php rendering
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- templates/people.php
+++
@@ -1,8 +0,0 @@
-<?php include "sections/header.php"; ?>
-<div class="container-narrow">
-	<div class="masthead">
-		<h3 class="muted">Blueridge</h3>
-	</div>
-	<hr>
-</div>
-<?php include "sections/footer.php"; ?>
