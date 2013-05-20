724d6405e67e5db10c5572f98862e8489f90d589
Moses Ngone <mo@mospired.com>
2013-05-20 10:46:05 -0400

    ad that view port resize line of code
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- app/views/sections/header.php
+++ app/views/sections/header.php
@@ -1,11 +1,12 @@
 <!doctype html>
 <html lang="en" ng-app="blueRidge">
 <head>
-  <meta charset="utf-8">
-  <title>BlueRidge App</title>
-  <link rel="stylesheet" href="/css/bootstrap.min.css"/>
-  <link rel="stylesheet" href="/css/app.css"/>
-  <link rel="stylesheet" href="/css/bootstrap-responsive.min.css"/>
+	<meta charset="utf-8">
+	<title>BlueRidge App</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="/css/app.css"/>
+	<link rel="stylesheet" href="/css/bootstrap-responsive.min.css"/>
 </head>
 <body>
-  <div id="wrap">
+	<div id="wrap">
