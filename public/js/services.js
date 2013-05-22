975b00a4a44294f053cf7ace77f128dc19454e6d
Moses Ngone <mo@mospired.com>
2013-05-22 11:28:15 -0400

    changing angular service calls
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- public/js/services.js
+++ public/js/services.js
@@ -3,12 +3,8 @@
 /* Services */
 angular.module('blueRidge.services', ['ngResource']).
 factory('Project', function($resource) {
-  var Project = $resource('http://dev-api.blueridgeapp.com/todos/',
-      { apiKey: '4f847ad3e4b08a2eed5f3b54' }, 
-      { 
-    	  'findMembers' : {method:'GET', isArray:true},
-    	  'findTodos' : {method:'GET', isArray:true},
-      });
 
+//$scope.Users = $resource("http://localhost/users/:id",{id:'@id'});
+  var Project = $resource('http://dev-api.blueridgeapp.com/todos/:id/',{id: '@id' });
   return Project;
 });
