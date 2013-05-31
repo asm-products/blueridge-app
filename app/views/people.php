2d5ecff05d065a43ca873f9dfb68cfdacb5451d5
Moses Ngone <mo@mospired.com>
2013-05-31 13:10:33 -0400

    views back to angular
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- app/views/people.php
+++
@@ -1,8 +0,0 @@
-<?php include "sections/header.php"; ?>
-<div class="container-narrow">
-	<div class="masthead">
-		<h3 class="muted">BlueRidge</h3>
-	</div>
-	<hr>
-</div>
-<?php include "sections/footer.php"; ?>
