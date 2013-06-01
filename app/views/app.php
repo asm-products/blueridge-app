9394830e72480131f4fe22a3f4b9988fd64d0233
Moses Ngone <mo@mospired.com>
2013-06-01 17:35:33 -0400

    used angular 1.1.5
    taking advantage of ng-if
    
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- app/views/app.php
+++ app/views/app.php
@@ -17,7 +17,7 @@
 			<p class="muted credit">&copy; 2013 &mdash; Made in Atlanta, GA by <a href="http://ninelabs.com">Nine Labs</a></p>
 		</div>
 	</div>
-	<script src="/lib/angular/angular.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.1.5/angular.min.js"></script>
 	<script src="/lib/angular/angular-resource.min.js"></script>
 	<script src="/js/app.js"></script>
 	<script src="/js/services.js"></script>
