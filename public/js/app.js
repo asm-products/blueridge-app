c9ec944ffadc73a803ff3f5c716177032a638833
Moses Ngone <mo@mospired.com>
2013-06-01 12:57:03 -0400

    new ToDos controller
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- public/js/app.js
+++ public/js/app.js
@@ -3,22 +3,28 @@ angular.module('blueRidge', ['blueRidge.services']).
 config(['$routeProvider',function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl: 'views/home.html', 
-		controller: 'HomeCtrl'
-	});
-		
-	$routeProvider.when('/todos', {
-		templateUrl: 'views/todos.html', 
-		controller: 'ToDoCtrl'
+		controller: 'HomeCtrl',
+		access:'access.public'
 	});
 	$routeProvider.when('/login', {
 		templateUrl: 'views/login.html', 
-		controller: 'LoginCtrl'
+		controller: 'LoginCtrl',
+		access:'access.public'
 	});
+
+	$routeProvider.when('/todos', {
+		templateUrl: 'views/todos.html', 
+		controller: 'ToDosController',
+		access:'access.user'
+	});
+
 	$routeProvider.when('/people', {
 		templateUrl: 'views/people.html', 
-		controller: 'PeopleCtrl'
+		controller: 'PeopleCtrl',
+		access:'access.user'
 	});
 	$routeProvider.otherwise({
 		redirectTo: '/'
 	});
-}]);
+}]).
+run();
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,17 +4,16 @@ function HomeCtrl($scope, Project) {
 	//$scope.members = Project.findUser();
 	//console.log($scope);
 }
-
-function ToDoCtrl($scope, Project) {
-	//$scope.todos = Project.findTodos();
-	//console.log($scope.todos);
+function ToDosController($scope, ToDos){
+	$scope.todos = ToDos.query();
+	console.log($scope.todos);
 }
 
 function PeopleCtrl($scope, Project) {
 	//$scope.members = Project.findMembers();
 	//console.log($scope.members);
 }
-function UsersCtrl($scope, Project) {
+function UsersCtrl($scope, Users) {
 	//$scope.members = Project.findUser();
 	//console.log($scope.members);
 }
