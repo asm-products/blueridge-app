1834ffa8502836fbfb1348b02601f76c7360ffa9
Moses Ngone <mo@mospired.com>
2013-06-17 22:56:53 -0400

    added an initialize view
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- app/js/app.js
+++ app/js/app.js
@@ -10,6 +10,10 @@ var blueRidgeApp = angular.module('blueRidgeApp', ['blueRidgeApp.controllers','b
 		templateUrl: 'views/settings.html', 
 		controller: 'SettingsCtrl'
 	})
+	.when('/init', {
+		templateUrl: 'views/sections/select-projects.html', 
+		controller: 'SettingsCtrl'
+	})
 	.when('/signout', {
 		templateUrl: 'views/home.html',
 		controller: 'SignOutCtrl'
