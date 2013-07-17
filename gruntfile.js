2a8edda94cae5cbabdb46fa701b0e0cf14190279
Moses Ngone <mo@mospired.com>
2013-07-16 23:27:17 -0400

    more grunt
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- gruntfile.js
+++ gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
       },
       dist: {
         files: ['<%= dir.src %>/**/*.*'],
-        tasks: ['build'],
+        tasks: ['publish'],
       },
     },
     copy: {
