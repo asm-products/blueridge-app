9bfefd88fc69f5322274a7f2226e98562de60fb2
Moses Ngone <mo@mospired.com>
2013-07-20 19:03:49 -0400

    resolved dependancies
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- gruntfile.js
+++ gruntfile.js
@@ -11,10 +11,9 @@ module.exports = function(grunt) {
     vendor: {
       js: [
       '<%= dir.vendor %>/jquery/jquery.min.js',
-      '<%= dir.vendor %>/angular.min/index.js',
+      '<%= dir.vendor %>/angular-all-unstable/angular.min.js',
+      '<%= dir.vendor %>/angular-all-unstable/angular-cookies.min.js',
       '<%= dir.vendor %>/angular-bootstrap/ui-bootstrap-tpls.min.js',
-      '<%= dir.vendor %>/angular-cookies/angular-cookies.min.js',
-      '<%= dir.vendor %>/angular-resource/angular-resource.min.js',
       '<%= dir.vendor %>/restangular/dist/restangular.min.js',
       '<%= dir.vendor %>/underscore/underscore-min.js',
       '<%= dir.vendor %>/angular-google-analytics/src/angular-google-analytics.js',
