505bdd1276a9d03d19eb2207b65f8028ef70391d
Moses Ngone <mo@mospired.com>
2013-07-23 00:49:32 -0400

    billing and such
    Signed-off-by: Moses Ngone <mo@mospired.com>

--- app/src/scripts/controllers/app/subscription.ctrl.js
+++ app/src/scripts/controllers/app/subscription.ctrl.js
@@ -4,24 +4,31 @@ angular.module('blueRidgeApp')
         $scope.cashier=cashier;
     });
 
+    $scope.alerts = [];
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+
     $scope.open=function(){
         $scope.shouldBeOpen = true;
         Stripe.setPublishableKey($scope.cashier.key);
 
     };
-    $scope.createToken = function(card){
-        Stripe.card.createToken(card, $scope.subscribe);
-        console.log(card);
-    };
-    $scope.subscribe = function(status, responce){
-        if (response.error) {
-            // show errors
-        } else {
-            
 
-        }
+    $scope.makePayment = function(card){
+        console.log(card);
         //Stripe.card.createToken(card, $scope.subscribe);
+    };
+
+    $scope.subscribe = function(status, response){
+
         console.log(status);
-        console.log(responce);
+        if (response.error) {
+            console.log(response.error); 
+            //$scope.alerts.push({ type: 'error', msg: response});
+        } else {
+            console.log(response);
+        }
     };
 });
