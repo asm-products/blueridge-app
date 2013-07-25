d6e12d45edfeb14971b5def25e088b6c528fcfcf
Moses Ngone <mngon@oncourselearning.com>
2013-07-25 04:45:02 -0400

    stripe backend
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- api/routes/subscriptions.php
+++ api/routes/subscriptions.php
@@ -1,40 +1,31 @@
 <?php
 /**
- * Subscriptions
+ * Subscriptiuserons
  */
+use \BlueRidge\Entities\User;
+
 $app->post('/api/subscriptions', function () use ($app) {   
 
     $params = json_decode($app->request()->getBody());
-    $code = $params->code;
-    $providerName = $params->provider;
 
-    if(isset($app->providers->$providerName)){
-        $handler  = "\\BlueRidge\\Providers\\{$app->providers->$providerName->handler}";
+    if(empty($params->user) || empty($params->payment) || empty($params->plan)){
+        $app->response()->status(402);
+        echo (json_encode((object) ['error'=>"Payment Required",'message'=>"Missing payment details"]));
     }else{
-        return ;
+
+        $user = new User($app);
+        $user->fetchOne(['id'=>$params->user]);
+        $customer = \cashier_subscribe($app,$user,$params);
+
+        if (!empty($customer)){
+            app->response()->status(200);
+            $user->update(["id"=>$user->id],['plan'=>$params->plan,'subscription'=>$customer],true);    
+            echo (json_encode((object) ['id'=>$user->id,'subscribed'=>true]));
+        }else{
+
+            $app->response()->status(404);
+            echo (json_encode((object) ['error'=>"Subscription failed",'message'=>"Contact BlueRidge Team"]));
+        }
     }
-
-    $provider = new $handler($app); 
-    $token = $provider->authorize($code);
-
-    $auth = $provider->getAuthorization($token);
-    $me= $provider->getMe($auth,$token);
-
-    $accounts = $provider->getProjectAccounts($auth,$token);
-
-
-    $user = new User($app);
-    $service_properties = ['providers'=>["{$providerName}"=>['auth'=>$auth]],'accounts'=>$accounts];
-    $properties = array_merge($me,$service_properties);
-    $resource = $user->create($properties);
-
-    echo (json_encode((object) ['id'=>$user->id,'init'=>true,'authorized'=>true]));
-
-    $access = doorman_welcome();
-    $user->update(["id"=>$user->id],['key'=>$access['key']],true);
-
-    
-    $mailman = \postman_send($app, $user,['password'=>$access['pass']]);    
-    
     
 });
