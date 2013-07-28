cde7ad020045ff3934633415b9f98a4f85146529
Moses Ngone <mngon@oncourselearning.com>
2013-07-28 01:09:06 -0400

    -n now we are double billing
    WTF???
    
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- src/scripts/controllers/app/profile.ctrl.js
+++ src/scripts/controllers/app/profile.ctrl.js
@@ -1,28 +1,51 @@
 angular.module('blueRidgeApp')
-.controller('ProfileCtrl',function($scope,$location,Auth,Restangular,PaymentHelpers){
+.controller('ProfileCtrl',function($rootScope,$scope,$location,Auth,Restangular,PaymentHelpers){
     if (!Auth.isSignedIn()) {
         $location.path('/');
     }
 
     $scope.alerts = [];
     
+
+
+    $scope.list = [];
+    $scope.text = 'hello';
+
+    $scope.card={};
+    $scope.subscribe = function() {
+
+        var subscriber = {
+            name:this.card.name,
+            number:this.card.number,
+            expiry:this.card.expiry
+        };
+
+        console.log(subscriber);
+
+        if (this.text) {
+            this.list.push(this.text);
+            this.text = '';
+        }
+    };
+
+
     $scope.closeAlert = function(index) {
         $scope.alerts.splice(index, 1);
     };
 
-    var blueRidgeUser = Restangular.one('users',Auth.currentUser()).get().then(function (user){
+    blueRidgeUser = Restangular.one('users',Auth.currentUser());
+    blueRidgeUser.get().then(function (user){
         $scope.user = user;
-        $scope.plan = $scope.user.plan;
+        $scope.plan = $scope.user.profile.plan;
         $scope.setPlan($scope.plan);
     });
 
-    var blueRidgeCashier = Restangular.one('services','cashier');
+    blueRidgeCashier = Restangular.one('services','cashier');
     blueRidgeCashier.get().then(function(stripe){
         Stripe.setPublishableKey(stripe.key);
     });
 
-    
-
+    blueRidgeSubscription = Restangular.all('subscriptions');
 
     $scope.setPlan = function(plan){
         switch (plan){
@@ -44,24 +67,16 @@ angular.module('blueRidgeApp')
             exp_month:expiry.month,
             exp_year:expiry.year
         };
-
-        Stripe.card.createToken(subscriber,$scope.subscribe);
+        Stripe.card.createToken(subscriber,function(status,response){
+            if (response.error) {
+                $scope.alerts.push({ type: 'error', msg: 'Oh my! '+response.error.message });
+            } else {
+                $rootScope.token = response.id;
+            }
+        });
     };
 
-    $scope.subscribe =  function(status,response){
 
-        if (response.error) {
-            $scope.alerts.push({ type: 'error', msg: 'Oh my! '+response.error.message });
-        } else {
-            var customer= {user:$scope.user.id, payment:response,plan:$scope.card.plan};
-            console.log(response);
-            console.log(customer);
 
-            Restangular.all('subscriptions').post(customer).then(function(result){
-                console.log('hello');
-                console.log(data);
-            });
-        }
 
-    };
 });
