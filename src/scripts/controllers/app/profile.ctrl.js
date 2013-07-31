226fff4db1aa7793951348dd3a0e72095a96dd36
Moses Ngone <mngon@oncourselearning.com>
2013-07-30 20:13:51 -0400

    -n no angular stripe no more
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- src/scripts/controllers/app/profile.ctrl.js
+++ src/scripts/controllers/app/profile.ctrl.js
@@ -1,9 +1,8 @@
 angular.module('blueRidgeApp')
-.controller('ProfileCtrl',function($rootScope,$scope,$location,Auth,Restangular,PaymentHelpers){
+.controller('ProfileCtrl',function($rootScope,$scope,$location,Auth,Restangular){
     if (!Auth.isSignedIn()) {
         $location.path('/');
     }
-
     blueRidgeUser = Restangular.one('users',Auth.currentUser());
     blueRidgeUser.get().then(function (user){
         $scope.user = user;
