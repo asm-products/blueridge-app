0fc30fd91db6ec524fa4245d3760d09398e91a93
Moses Ngone <mngon@oncourselearning.com>
2013-07-31 15:21:35 -0400

    -n no stripe new ui
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- src/scripts/controllers/app/profile.ctrl.js
+++ src/scripts/controllers/app/profile.ctrl.js
@@ -3,9 +3,18 @@ angular.module('blueRidgeApp')
     if (!Auth.isSignedIn()) {
         $location.path('/');
     }
+
     blueRidgeUser = Restangular.one('users',Auth.currentUser());
     blueRidgeUser.get().then(function (user){
         $scope.user = user;
+        $scope.plan = $scope.user.profile.plan;
     });
+
+    $scope.getCurrentPlan = function (){
+
+        if (typeof($scope.plan) != "undefined"){
+            return '/views/app/sections/plan-'+$scope.plan+'.html';           
+       }
+   };
 
 });
