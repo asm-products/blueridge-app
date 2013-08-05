d60964888416766e164f28f61c4bfb03ffbc71d5
Moses Ngone <mngon@oncourselearning.com>
2013-08-05 17:45:48 -0400

    -n added profile selectors
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- src/scripts/controllers/app/profile.ctrl.js
+++ src/scripts/controllers/app/profile.ctrl.js
@@ -1,55 +1,23 @@
-blueRidgeApp.controller('ProfileCtrl',['$scope','$route','$location','Auth','Restangular','$cookieStore',function ProfileCtrl($scope,$location,$route,Auth,Restangular,$cookieStore){
+blueRidgeApp.controller('ProfileCtrl',['$scope','$location','Auth','Restangular',function ProfileCtrl($scope,$location,Auth,Restangular){
     if (!Auth.isSignedIn()) {
         $location.path('/');
     }
 
-    var safeApply = function safeApply(fn) {
-        if($scope.$$phase || $scope.$root.$$phase){
-            fn();
-        }else{
-            $scope.$apply(fn);
-        }
-    };
-
     blueRidgeUser = Restangular.one('users',Auth.currentUser());
-    blueRidgeUser.get().then(function (user){
-        $scope.user = user;
-        $scope.plan = $scope.user.profile.plan;
+    $scope.user = blueRidgeUser.get();
+
+    blueRidgeUser.one('subscription').get().then(function (result){
+        $scope.subscription = result.subscription;        
     });
 
-    Restangular.one('services','cashier').get().then(function(stripe){
-        $scope.stripe = stripe;
-    });
-
-
-    $scope.makePayment = function makePayment(token,plan) {
-        safeApply(function() {
-            $scope.wait = true;
-        });
-
-        var customer= {
-            user:$scope.user.id,
-            token:token.id,
-            plan:plan
-        };
-
-        console.log(customer);
-
-        Restangular.all('subscriptions').post(customer).then(
-            function success (resource){
-                safeApply(function() {
-                    $scope.wait = false;
-                       //$location.url('/invoice/' + resource._id);
-                });
-                console.log('result from api');
-                console.log(resource);
-            },
-            function error(){
-                console.log('There was an error saving');
-                safeApply(function() {
-                    $scope.wait = true;
-                    $route.reload();
-                });
-            });
+    $scope.changePlan = function changePlan(plan){
+        $location.path('/app/cart/'+plan);
     };
+    $scope.isCurrentPlan = function isCurrentPlan(plan){
+        if( $scope.subscription.plan == plan){
+            return true;
+        }
+        return false;
+    };
+
 }]);
