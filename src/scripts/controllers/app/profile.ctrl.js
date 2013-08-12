8ac354cdd54192f02d42f5dffb7c01b1865e41cf
Moses Ngone <mngon@oncourselearning.com>
2013-08-12 00:31:03 -0400

    -n upgrade downgrade
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- src/scripts/controllers/app/profile.ctrl.js
+++ src/scripts/controllers/app/profile.ctrl.js
@@ -2,19 +2,24 @@ blueRidgeApp.controller('ProfileCtrl',['$scope','$location','Auth','Restangular'
     if (!Auth.isSignedIn()) {
         $location.path('/');
     }
-    $scope.subscription={};
+    
     blueRidgeUser = Restangular.one('users',Auth.currentUser());
     $scope.user = blueRidgeUser.get();
+    $scope.subscription={
+        plan:{id:'',name:''},
+        cards:[]
+    };
 
-    blueRidgeUser.one('subscription').get().then(function (result){
-        $scope.subscription = result.subscription;        
+    blueRidgeUser.one('subscription').get().then(function(subscription){
+        $scope.subscription.plan=subscription.plan;
+        $scope.subscription.cards=subscription.cards;
     });
 
     $scope.changePlan = function changePlan(plan){
         $location.path('/app/cart/'+plan);
     };
     $scope.isCurrentPlan = function isCurrentPlan(plan){
-        if($scope.subscription.plan == plan){
+        if($scope.subscription.plan.id == plan){
             return true;
         }
         return false;
