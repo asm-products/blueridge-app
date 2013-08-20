43b3dfa6440108817edb7109423941c7941241ea
J Cornelius <github@jcornelius.com>
2013-08-19 22:30:41 -0400

    -n Removed (/) in routes.
    Signed-off-by: J Cornelius <github@jcornelius.com>

--- routes/site.php
+++ routes/site.php
@@ -10,18 +10,18 @@ $app->get('/preview/',function() use ($app){
     $app->render('site/preview.html');
 });
 
-$app->get('/pricing(/)',function() use ($app){
+$app->get('/pricing/',function() use ($app){
     $app->render('site/pricing.html');
 });
 
-$app->get('/privacy(/)',function() use ($app){
+$app->get('/privacy/',function() use ($app){
     $app->render('site/privacy.html');
 });
 
-$app->get('/about(/)',function() use ($app){
+$app->get('/about/',function() use ($app){
     $app->render('site/about.html');
 });
 
-$app->get('/support(/)',function() use ($app){
+$app->get('/support/',function() use ($app){
     $app->render('site/support.html');
 });
