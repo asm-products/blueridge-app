c1791efde798f0a45281ee34e438f7736f50e0b9
J Cornelius <github@jcornelius.com>
2013-08-19 23:52:13 -0400

    -n Removed superfluous routes.
    Signed-off-by: J Cornelius <github@jcornelius.com>

--- routes/site.php
+++ routes/site.php
@@ -6,32 +6,8 @@ $app->get('/',function() use ($app){
     $app->render('site/home.html');
 });
 
-$app->get('/preview/',function() use ($app){
-    $app->render('site/preview.html');
-});
-
-$app->get('/pricing/',function() use ($app){
-    $app->render('site/pricing.html');
-});
-
-$app->get('/privacy/',function() use ($app){
-    $app->render('site/privacy.html');
-});
-
-$app->get('/about/',function() use ($app){
-    $app->render('site/about.html');
-});
-
-$app->get('/support/',function() use ($app){
-    $app->render('site/support.html');
-});
-
-$app->get('/signin/',function() use ($app){
-    $app->render('site/signin.html');
-});
-
-$app->get('/signout/',function() use ($app){
-    $app->render('site/signout.html');
+$app->get('/(:route/)',function($route) use ($app){
+    $app->render("site/{$route}.html");
 });
 
 $app->response->headers->set('Content-Type', 'text/html');
