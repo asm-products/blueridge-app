e395c951667b0bd2749e1034ed82916f5c9880e3
J Cornelius <github@jcornelius.com>
2013-08-19 22:29:40 -0400

    Website is done.
    Signed-off-by: J Cornelius <github@jcornelius.com>

--- app/routes/site.php
+++ app/routes/site.php
@@ -9,3 +9,19 @@ $app->get('/',function() use ($app){
 $app->get('/preview(/)',function() use ($app){
     $app->render('site/preview.html');
 });
+
+$app->get('/pricing(/)',function() use ($app){
+    $app->render('site/pricing.html');
+});
+
+$app->get('/privacy(/)',function() use ($app){
+    $app->render('site/privacy.html');
+});
+
+$app->get('/about(/)',function() use ($app){
+    $app->render('site/about.html');
+});
+
+$app->get('/support(/)',function() use ($app){
+    $app->render('site/support.html');
+});
