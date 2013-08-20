8913c13e8073b73964636d498098df9f9b910f92
Moses Ngone <mngon@oncourselearning.com>
2013-08-19 20:49:39 -0400

    spankin new box

---
+++ devbox/init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e # Exit script immediately on first error.
+set -x # Print commands and their arguments as they are executed.
+
+
+# Start Apache
+sudo service apache2 start
+
+# Start Apache
+#sudo service mongodb start
