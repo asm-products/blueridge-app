8efc8bd742e72cd3a7d2510d51ee4d6407ee3a2d
Moses Ngone <mngon@oncourselearning.com>
2013-08-20 00:45:40 -0400

    configs and entities

--- app/resources/index.php
+++ app/resources/index.php
@@ -5,6 +5,7 @@
 require '../vendor/autoload.php';
 use \Slim\Slim;
 use \Slim\Views;
+use \BlueRidge\Init;
 
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../app'));
 defined('CACHE_DIR') || define('CACHE_DIR', realpath(dirname(__FILE__).'/../cache'));
@@ -14,6 +15,9 @@ $app = new Slim([
     'view' => new Views\Twig(),
     'templates.path' => APPLICATION_PATH.'/resources/views'
     ]);
+$app->setName('blueridgeapp');
+$app->add(new Init());
+
 
 $view = $app->view();
 $view->parserOptions = ['debug' => true,'cache' => CACHE_DIR];
