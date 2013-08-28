84bfe2d39e3234615013426a9b09789a6e8c69d3
Moses Ngone <mngon@oncourselearning.com>
2013-08-28 10:21:19 -0700

    -n projects selection .. done
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- routes/app/projects.php
+++ routes/app/projects.php
@@ -10,13 +10,16 @@ $app->get('/app/projects/',$authenticate($app), function () use ($app) {
     $id = $_SESSION['user'];
     $user= new User($app);
     $projects = $user->fetchOne($id)->projects;
-    var_dump($projects);
-    exit();
-    //->fetchSegment('projects');
-    $app->render("app/projects.html", ['projects' => $user->projects,'route'=>'projects']);    
+    $app->render("app/projects.html", ['projects' => $projects,'route'=>'projects']);    
 });
 
-$app->post('/app/projects',$authenticate($app),function() use ($app){
-    error_log('form post');
+$app->post('/app/projects/',$authenticate($app),function() use ($app){
 
+    $params = $app->request->post('selected');
+    $id = $_SESSION['user'];
+    $user= new User($app);
+    $user->fetchOne($id);
+    $user->updateProfile('projects',$params);
+    $user->save();
+    $app->redirect('/app/todos/');
 });
