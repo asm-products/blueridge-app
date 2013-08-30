0fcf71871b84380142b9be8a5323e627449bf0ba
Moses Ngone <mngon@oncourselearning.com>
2013-08-30 02:34:17 -0400

    new service and provider descriptors
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- app/src/Blueridge/Init/Services/Mail.php
+++ app/src/Blueridge/Init/Services/Mail.php
@@ -5,18 +5,18 @@
  * @author Moses Ngone <mo@mospired.com> @mosesngone
  */
 
-namespace BlueRidge\Init;
+namespace BlueRidge\Init\Services;
 
 use \Slim\Middleware;
 
-class Mailbox extends Middleware
+class Mail extends Middleware
 {
     public function call()
     {
         if(!empty($this->app->config('services')['mail']))
         {            
-            $mailbox = $this->app->config('services')['mail'];
-            $this->app->mailbox =  new \Mandrill($mailbox['api_key']);
+            $mail = $this->app->config('services')['mail'];
+            $this->app->mail =  new \Mandrill($mail['api_key']);
         }
         $this->next->call();
 
