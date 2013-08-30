3b38bc7eec0a7816190f18963904e5a1e507c9fb
Moses Ngone <mngon@oncourselearning.com>
2013-08-30 02:33:36 -0400

    -n New doctrine document mappers
    Signed-off-by: Moses Ngone <mngon@oncourselearning.com>

--- src/Blueridge/Init/Db.php
+++ src/Blueridge/Init/Db.php
@@ -8,25 +8,36 @@
 namespace BlueRidge\Init;
 
 use \Slim\Middleware;
+use \Doctrine\MongoDB\Connection;
+use \Doctrine\ODM\MongoDB\Configuration;
+use \Doctrine\ODM\MongoDB\DocumentManager;
+use \Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 
 class Db extends Middleware
 {
     public function call()
     {
-                
         if(!empty($this->app->config('database')))
-        {
+        {   
             $this->app->container->singleton('db', function () {
-                $db_connect = $this->app->config('database');
-                $connection_url = "mongodb://{$db_connect['host']}:{$db_connect['port']}/{$db_connect['name']}";
+                $configs = $this->app->config('database');
+                $connection_url = "mongodb://{$configs['host']}:{$configs['port']}/{$configs['name']}";
+                AnnotationDriver::registerAnnotationClasses();
+
+                $config = new Configuration();
+                $config->setProxyDir(APPLICATION_PATH.'/src/BlueRidge/Documents/proxies');
+                $config->setProxyNamespace('Proxies');
+                $config->setHydratorDir(APPLICATION_PATH.'/src/BlueRidge/Documents/hydrators');
+                $config->setHydratorNamespace('Hydrators');
+                $config->setMetadataDriverImpl(AnnotationDriver::create(APPLICATION_PATH.'/src/BlueRidge/Documents'));
+                $config->setDefaultDB($configs['name']);
+
                 $client= new \MongoClient($connection_url);
-                return $client->selectDB($db_connect['name']);
+
+                return  DocumentManager::create( new Connection($client), $config);
+                
             });
         }
-
         $this->next->call();
-
     }
-
-
 }
