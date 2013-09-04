8787c0a3b7908dba480d5cfcaf2e1530aec170c2
Moses Ngone <mngon@oncourselearning.com>
2013-09-04 19:30:50 -0400

    -n better filtering of views

---
+++ routes/site.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Routes for the Application
+ */
+
+
+$app->get('/:page/',function($page) use ($app){
+
+    $allowed_routes = ['pricing','preview','privacy','about'];
+    if(in_array($page, $allowed_routes))
+    {
+        $app->render("site/{$page}.html", array('routeName' => $page));    
+    }else
+    {
+       $app->render("common/error-404.html",['message'=>'No Joy. File not found',404]);
+   }
+
+
+});
+$app->response->headers->set('Content-Type', 'text/html');
