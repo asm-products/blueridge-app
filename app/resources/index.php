5d0399518c6abe6bb0f83c5782149fec1959c278
Moses Ngone <mngon@oncourselearning.com>
2013-09-07 08:36:10 -0400

    setup cache driver

--- app/resources/index.php
+++ app/resources/index.php
@@ -10,6 +10,7 @@ use \Slim\Middleware\SessionCookie;
 
 use \BlueRidge\Middleware\Bootstrap;
 use \BlueRidge\Middleware\Db;
+use \BlueRidge\Middleware\Cache;
 use \BlueRidge\Middleware\Mail;
 use \BlueRidge\Middleware\View;
 use \BlueRidge\Middleware\Subscription;
@@ -22,6 +23,7 @@ $app = new Slim();
 $app->setName('blueridgeapp');
 $app->add(new SessionCookie(['secret' => '4VtUZrv8@Y','name'=>'_blrdgapp','expires'=>'24 hours']));
 $app->add(new Db());
+$app->add(new Cache());
 $app->add(new Mail());
 $app->add(new View());
 $app->add(new Bootstrap());
--- app/src/Blueridge/Middleware/Cache.php
+++ app/src/Blueridge/Middleware/Cache.php
@@ -1,4 +1,19 @@
 <?php
 /**
- * Instantiate Cache
+ * Cache
+ * @author Moses Ngone <mo@mospired.com> @mosesngone
  */
+
+namespace BlueRidge\Middleware;
+
+use \Slim\Middleware;
+use \Doctrine\Common\Cache\ApcCache;
+
+class Cache extends Middleware
+{
+    public function call()
+    {
+        $this->app->cache = new ApcCache();
+        $this->next->call();
+    }
+}
