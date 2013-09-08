046ce4800a1704c4b2b9c4c9be8eefdb62b5c4db
Moses Ngone <mngon@oncourselearning.com>
2013-09-08 10:26:35 -0400

    update for guzzle

--- app/routes/app/todos.php
+++ app/routes/app/todos.php
@@ -4,16 +4,15 @@
  */
 
 use \BlueRidge\Documents\User;
-use \BlueRidge\Entities\Todo;
+use \BlueRidge\Providers\Basecamp\BasecampClient;
 
 $app->get('/app/todos/',$authenticate($app), function () use ($app) {
 
     $id = $_SESSION['user'];
     $user = $app->dm->find('\BlueRidge\Documents\User', $id);
 
-
-    $todo = new Todo($app);
-    $todos = $todo->fetchByUser($user);
+    $basecampClient = BasecampClient::factory($app)->setAuth($user);
+    $todos = $basecampClient->getTodos($user);
 
     $app->render("app/todos.html", ['todos' => $todos,'route'=>'todos']);    
 });
