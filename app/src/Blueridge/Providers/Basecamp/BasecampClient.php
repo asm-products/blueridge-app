c70f26a27b63ac1cc4dabe45897f40af7efeb30b
Moses Ngone <mngon@oncourselearning.com>
2013-09-16 14:54:06 -0400

    backend cache and new base camp client

--- app/src/Blueridge/Providers/Basecamp/BasecampClient.php
+++ app/src/Blueridge/Providers/Basecamp/BasecampClient.php
@@ -1,334 +1,75 @@
 <?php
 /**
- * Basecamp Auth classes
+ * Basecamp Client
  */
 
 namespace Blueridge\Providers\Basecamp;
 
-use \Blueridge\Documents\User;
+use \Doctrine\Common\Cache\FilesystemCache;
+use \Guzzle\Common\Collection;
+use \Guzzle\Service\Client;
+use \Guzzle\Service\Description\ServiceDescription;
 use \Guzzle\Common\Event;
+use \Guzzle\Common\Exception\InvalidArgumentException;
+use \Guzzle\Cache\DoctrineCacheAdapter;
+use \Guzzle\Plugin\Cache\CachePlugin;
 
-class BasecampClient
+
+class BasecampClient extends Client
 {
+    /**
+     * @param array $config
+     * @return \Guzzle\Service\Client|BasecampClient
+     * @throws \Guzzle\Common\Exception\InvalidArgumentException
+     */
+    public static function factory($config = array())
+    {
 
-	protected $user_agent;
-	protected $client_id;
-	protected $client_secret;
-	protected $redirect_uri;
-	protected $auth_url;
-	protected $token_url;
-	protected $token;
-	protected $accounts =[];
-	protected $identity;
-	protected $expires_at;
-	protected $handler;
+        $default = array(
+            'base_url'      => 'https://basecamp.com/2011887/api/{version}/',
+            'version'       => 'v1',
+            'auth_method'   => 'oauth',
+            'access_token'  => null
+            );
+        $required = array('user_agent');
+        $config = Collection::fromConfig($config, $default, $required);
+        $client = new self($config->get('base_url'), $config);
 
-	public function __construct($settings)
-	{		
-		$this->setProperties($settings);
-	}
 
-	public static function factory($app)
-	{
-		$properties = $app->config('providers')['basecamp'];
-		$handler = $app->provider;
-		$properties['handler'] = $handler;
-		$client = new self($properties);
+        /**
+         * Attach Access Token
+         */
+        if (! isset($config['access_token'])) {
+            throw new InvalidArgumentException("Config must contain token when using oath");
+        }
+        $authoritzation = sprintf('Bearer %s', $config['access_token']);
+        
+        if (! isset($authoritzation)) {
+            throw new InvalidArgumentException("Config must contain valid authentication method");
+        }
 
-		return $client;
-	}
+        /**
+         * Attach Cache Plugin
+         */
+        $cachePlugin = new CachePlugin([
+            'adapter' => new DoctrineCacheAdapter(new FilesystemCache(CACHE_DIR.'/basecamp'))
+            ]);
 
-	/**
-	 * Set Properties
-	 */
-	protected function setProperties($properties)
-	{
-		
-		foreach($properties as $property => $value){
-			if (property_exists($this, $property)) {
-				if($property == "auth_url"){
-					$this->auth_url = $this->getAuthUrl($properties);
-				}else{
-					$this->$property = $value;
-				}
+        $client->addSubscriber($cachePlugin);
 
-			}
-		}
-	}
-	
-	/**
-	 * Get Token
-	 */
-	public function getToken ($code,$refresh=null)
-	{
+        // Attach a service description to the client
+        // $description = ServiceDescription::factory(__DIR__ . '/service.php');
+        // $client->setDescription($description);
 
-		$params = [
-		'type'=>'web_server',
-		'client_id'=>$this->client_id,
-		'redirect_uri'=>$this->redirect_uri,
-		'client_secret'=>$this->client_secret,
-		'code'=>$code
-		];
+        // Set required User-Agent
+        $client->setUserAgent($config['user_agent']);
+        
 
-		$request = $this->handler->post($this->token_url,[],$params);
-		$response = $request->send();
-		$this->token = $response->json();
-		return $this;
-	}
+        $client->getEventDispatcher()->addListener('request.before_send', function(Event $event) use ($authoritzation) {
+            $event['request']->addHeader('Authorization', $authoritzation);
 
-	/**
-	 * Get Authorization
-	 */
-	public function getAuthorization()
-	{
-		$endpoint="https://launchpad.37signals.com/authorization.json";		
-		$this->setAuth();
-		$request = $this->handler->get($endpoint);
-		$response = $request->send();
-		$auth = $response->json();
+        });
 
-		$this->identity = $auth['identity'];
-		$this->expires_at = $auth['expires_at'];
-
-		$authIterator = new \ArrayIterator($auth['accounts']);
-		foreach ($authIterator as $account) {
-			if($account['product'] =='bcx'){
-				$this->accounts[] = $account;
-			}
-		}		
-		return $this;
-	}
-	
-	/**
-	 * Get Me
-	 */
-	public function getMe()
-	{
-		$endpoint = "people/me.json";
-		$url="{$this->accounts[0]['href']}/{$endpoint}";
-		
-		$request = $this->handler->get($url);
-		$response = $request->send();
-		$whoami = $response->json();
-		
-		$avatar = parse_url($whoami['avatar_url']);
-		return [
-		'name'=>$whoami['name'],
-		'firstName'=>$this->identity['first_name'],
-		'lastName'=>$this->identity['last_name'],
-		'email'=>$whoami['email_address'],
-		'avatar'=>"//{$avatar['host']}/{$avatar['path']}?{$avatar['query']}"
-		];
-	}
-
-	/**
-	 * Get Projects
-	 */
-	public function getProjects()
-	{
-		$endpoint="projects.json";
-		$projects=[];
-		$index=0;
-
-		$accountIterator = new \RecursiveArrayIterator($this->accounts);
-		foreach (new \RecursiveArrayIterator($accountIterator) as $key => $account) {
-			$request = $this->handler->get("{$account['href']}/{$endpoint}");
-			$response = $request->send();
-			$data= $response->json();
-
-			if(!empty($data)){
-				$projectIterator = new \ArrayIterator($data);
-				foreach($projectIterator as $project){
-					$names[$index] = $project['name'];
-					$index++;
-					$project['account']=$account;
-					$projects[] = $project;
-				}
-			}
-		}
-		return $projects;
-	}
-
-	/**
-	 * Get Auth Url
-	 */
-	private function getAuthUrl($properties){
-		$redirect_uri = urlencode($properties['redirect_uri']);
-		return "{$properties['auth_url']}?client_id={$properties['client_id']}&redirect_uri={$redirect_uri}&type=web_server";
-	}
-
-	/**
-	 * Get Profile Projects
-	 */
-	public function getProfileProjects(User $user)
-	{
-
-		if(empty($user->profile['projects'])){
-			return;
-		}
-		
-		$projectIterator = new \ArrayIterator($user->projects);
-		$profileProjects = [];
-
-		foreach($projectIterator as $project){
-			if(in_array($project['id'], $user->profile['projects']))
-			{
-				$base= $project['account']['href'];
-				$profileProjects[] = ['baseUrl'=>$base,'projectId'=>$project['id'],'name'=>$project['name']];
-			}
-		}
-		return $profileProjects;
-	}
-
-	/**
-	 * Get Todos
-	 */
-	public function getTodos(User $user)
-	{
-		// get profile projects
-		$profileProjects = $this->getProfileProjects($user);
-
-		// get todo lists
-		$todolists = $this->getTodoLists($user, $profileProjects);
-		
-		// get the todos
-		$todos = [];
-		$todolistIterator = new \RecursiveArrayIterator($todolists);
-		foreach (new \RecursiveArrayIterator($todolistIterator)  as $list) {
-
-			$request = $this->handler->get($list['url']);
-			$response = $request->send();
-			$data = $response->json();
-			$incomplete_todos = $data['todos']['remaining'];			
-			$inherited = ['project'=>$list['project'],'list'=>$list['name']];
-
-			array_walk($incomplete_todos, function(&$a, $key, $inherited) {				
-				$a['project'] = $inherited['project'];
-				$a['list'] = $inherited['list'];
-				$a['href'] = BasecampClientHelper::getSiteUrl($a['url']);				
-			},$inherited);
-
-			$todos = array_merge($todos,$incomplete_todos);
-		}
-		return BasecampClientHelper::organizeTodos($todos);
-	}
-
-	/**
-	 * Get Todolists
-	 */
-	public function getTodoLists(User $user, $profileProjects){	
-
-		$projectsIterator = new \ArrayIterator($profileProjects);
-
-		$todoLists=array();
-		$list= array();
-		foreach ($projectsIterator as $project) {
-			$project_name = $project['name'];
-			$endpoint = "{$project['baseUrl']}/projects/{$project['projectId']}/todolists.json";
-			$request = $this->handler->get($endpoint);
-			$response = $request->send();
-			$todolist = $response->json();
-
-			array_walk($todolist, function(&$a, $key, $project_name) {				
-				$a['project'] = $project_name;
-			},$project_name);
-
-			$todoLists=array_merge($todoLists, $todolist);
-		}
-		
-		return $todoLists;
-	}
-
-	/**
-	 * Set User Authorization
-	 */
-	public function setAuth($settings = null)
-	{
-		if (!empty($settings))
-		{
-			$this->setProperties($settings);			
-		}
-
-		$authorization ="Bearer {$this->token['access_token']}";			
-		$this->handler->getEventDispatcher()->addListener('request.before_send', function(Event $event) use ($authorization) {
-			$event['request']->addHeader('Authorization', $authorization);
-		});
-		return $this;
-	}
-
-	/**
-	 * Get Data
-	 * @deprecated
-	 */
-	// private function getData($url){
-
-	// 	$ch = curl_init();
-	// 	curl_setopt($ch, CURLOPT_URL, $url);
-	// 	curl_setopt($ch, CURLOPT_HEADER, 0);
-	// 	curl_setopt($ch, CURLOPT_USERAGENT, $this->user_agent);
-	// 	if(!empty($this->token)){
-	// 		curl_setopt($ch, CURLOPT_HTTPHEADER,["Authorization: Bearer {$this->token['access_token']}"]);
-	// 	}		
-	// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	// 	$data=curl_exec($ch);
-	// 	curl_close($ch);
-	// 	return json_decode($data,true);
-
-	// }
-
-	/**
-	 * Post Data
-	 * Post data to basecamp
-	 * @deprecated
-	 */
-	// private function postData($url,Array $params,$token=null){
-
-	// 	$params = http_build_query($params);
-	// 	$ch = curl_init();
-	// 	curl_setopt($ch, CURLOPT_URL, $url);
-	// 	curl_setopt($ch, CURLOPT_POST, 1);
-	// 	curl_setopt($ch, CURLOPT_POSTFIELDS, $params );
-	// 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
-	// 	curl_setopt($ch, CURLOPT_USERAGENT, $this->user_agent);
-	// 	if(!empty($this->token)){
-	// 		curl_setopt($ch, CURLOPT_HTTPHEADER,["Authorization: Bearer {$this->token['access_token']}"]);
-	// 	}	
-	// 	curl_setopt($ch, CURLOPT_HEADER,0);  
-	// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER ,1); 
-	// 	$data = curl_exec($ch);
-	// 	curl_close($ch);
-
-	// 	return json_decode($data,true);
-
-	// }
-	public function getProperties()
-	{
-		$item = [
-		"token"=>$this->token,
-		"accounts"=>$this->accounts,
-		"identity"=>$this->identity
-		];
-		return $item;
-	}
-
-	public function toArray(){
-		$item = ["name"=>$this->name,"authUrl"=>$this->authUrl];
-		return $item;
-	}
-
-	public function __get($property)
-	{
-		if (property_exists($this, $property)) {
-			return $this->$property;
-		}
-	}
-	
-	public function __set($property, $value)
-	{
-		if (property_exists($this, $property)) {
-			$this->$property = $value;
-		}
-		return $this;
-	}
-
+        return $client;
+    }
 }
