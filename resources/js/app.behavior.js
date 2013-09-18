7dd5b03a75cd1609b4da517a06ed783136ec80ed
Moses Ngone <mngon@oncourselearning.com>
2013-09-17 22:48:35 -0400

    -n tabs and spaces

--- resources/js/app.behavior.js
+++ resources/js/app.behavior.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 	$('.app-navigation .nav-link').click(function(){
 		var img = new Image();
-  	img.src = $('#loading-bar').attr('src');
+		img.src = $('#loading-bar').attr('src');
 		$(this).parent('li').addClass('active');
 		$(this).parent('li').siblings().removeClass('active');
 		$('#loading-message p').html($(this).data('message'));
