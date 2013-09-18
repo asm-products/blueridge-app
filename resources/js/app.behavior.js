168eb5015b15781662183b8d6997ef06e637efba
J Cornelius <github@jcornelius.com>
2013-09-17 22:15:09 -0400

    -n Cleaned up the Profile a bit and made the loading messages better.
    Signed-off-by: J Cornelius <github@jcornelius.com>

--- resources/js/app.behavior.js
+++ resources/js/app.behavior.js
@@ -3,9 +3,12 @@
 $(document).ready(function(){
 
 	$('.app-navigation .nav-link').click(function(){
-		$('.panel').html($('#loading-message').html());
+		var img = new Image();
+  	img.src = $('#loading-bar').attr('src');
 		$(this).parent('li').addClass('active');
 		$(this).parent('li').siblings().removeClass('active');
+		$('#loading-message p').html($(this).data('message'));
+		$('.panel').html($('#loading-message').html());
 	});
 
 	var plan_limits = {
@@ -24,8 +27,6 @@ $(document).ready(function(){
 			$('#plan-limit-modal').modal();
 		}
 	});
-
-
 
 	/**
 	* Profile Page
