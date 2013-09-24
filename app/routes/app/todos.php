54221fe8f6c4f3dcf3a15021ba5bf91f5440d647
J Cornelius <github@jcornelius.com>
2013-09-24 13:43:59 -0400

    Starting to implement sorting and filtering, but it's buggy.
    Filter operations work fine until you do a sort operation.
    
    Signed-off-by: J Cornelius <github@jcornelius.com>

--- app/routes/app/todos.php
+++ app/routes/app/todos.php
@@ -11,8 +11,8 @@ $app->get('/app/todos/',$authenticate($app), function () use ($app) {
 
     $id = $_SESSION['user'];
     $user = $app->dm->find('\Blueridge\Documents\User', $id);
-    
+
     $todos = BasecampClientHelper::getTodos($app,$user);
-    $app->render("app/todos.html", ['todos' => $todos,'route'=>'todos']);    
+    $app->render("app/todos.html", ['todos' => $todos,'route'=>'todos']);
 });
 
