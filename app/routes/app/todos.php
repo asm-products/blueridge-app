2612a55c1fb10f94338d0222c122ed701b33319d
Moses Ngone <mngon@oncourselearning.com>
2013-10-03 11:35:00 -0400

    fixed unused sessions
    Conflicts:
    	app/routes/app/todos.php

--- app/routes/app/todos.php
+++ app/routes/app/todos.php
@@ -11,8 +11,15 @@ $app->get('/app/todos/',$authenticate($app), function () use ($app) {
 
     $id = base64_decode($_SESSION['user']);
     $user = $app->dm->find('\Blueridge\Documents\User', $id);
+
+
+    if (empty($user)){
+        unset($_SESSION['live']); 
+        unset($_SESSION['user']);
+        $app->redirect('/');  
+    }
     
     $todos = BasecampClientHelper::getTodos($app,$user);
-    $app->render("app/todos.html", ['todos' => $todos,'route'=>'todos']);    
+    $app->render("app/todos.html", ['user' =>$user->toArray(),'todos' => $todos,'route'=>'todos']);
 });
 
