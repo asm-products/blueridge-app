0f1c422186774effac5b223a2b6c974c94ce4a40
Moses Ngone <moses@mospired.com>
2013-12-18 05:57:58 -0500

    updated Doorman to minimal static calls

--- bin/src/Blueridge/Utilities/Doorman.php
+++ bin/src/Blueridge/Utilities/Doorman.php
@@ -1,31 +1,56 @@
 <?php
 /**
- * Doorman
+ * Mapster
+ *
+ * @copyright Ninelabs 2013
+ * @author Moses Ngone <moses@ninelabs.com>
+ * @since v0.1.0
  */
 
 namespace Blueridge\Utilities;
 
+/**
+ * Doorman
+ * 
+ * Doorman helps you generate and validate keys and access codes
+ * @package Blueridge
+ */
 class Doorman
 {
-    public static function getAccess($password = null)
+    /**
+     * get a generated code
+     * @param  String $code Can be null
+     * @return Array
+     */
+    public static function getCode($code = null)
     {
-        if(empty($password)){
-            $password = self::generatePassword();    
+        if(empty($code)){
+            $code = self::generateCode();    
         }
-        
-        $key = password_hash($password, PASSWORD_BCRYPT);
-        return ['key'=>$key,'password'=>$password];
+
+        $key = password_hash($code, PASSWORD_BCRYPT);
+        return ['key'=>$key,'code'=>$code];
+    }
+    
+    /**
+     * Validate code against key
+     * 
+     * @param String $code
+     * @param String $key
+     */
+    public static function validate($code,$key)
+    {
+        return password_verify($code, $key);
     }
 
-    public static function authorize($password,$key)
+    /**
+     * Generate a 16 digit random code
+     * @return String code
+     */
+    public static function generateCode()
     {
-        return password_verify($password, $key); 
-    }
-
-    public static function generatePassword()
-    {
-        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&-_";           
-        $password = substr(str_shuffle( $chars ), 0, 10 );
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#-_&%$*(){}[]=+;:";           
+        $password = substr(str_shuffle( $chars ), 0, 16 );
         return $password;
     }
 }
