4c70a6ea27b8988f006c7b2199e0a236d04491c8
Moses Ngone <moses@mospired.com>
2013-12-18 06:00:49 -0500

    Changed route locations

--- app/init.php
+++ app/init.php
@@ -1,14 +1,18 @@
 <?php
 /**
+ * Blueridge 
+ * 
  * Application routes
+ * @copyright Ninelabs 2013
+ * @author Moses Ngone <moses@ninelbas.com>
+ * @since 0.1.0
  */
 
-require "routes/auth/connect.php";
-require "routes/auth/basecamp.php";
-require "routes/app/export.php";
-require "routes/app/projects.php";
-require "routes/app/todos.php";
-require "routes/app/profile.php";
-require "routes/app/cart.php";
-require "routes/app/sign-out.php";
+require "routes/basecamp.php";
+require "routes/export.php";
+require "routes/projects.php";
+require "routes/todos.php";
+require "routes/profile.php";
+require "routes/cart.php";
+require "routes/sign-out.php";
 require "routes/site.php";
