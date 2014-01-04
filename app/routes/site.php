6d3fb5f8f94c3310f3bc923b72129e8594235eb7
Moses Ngone <moses@mospired.com>
2014-01-04 12:37:30 -0500

    api routes updated and response type headers

--- app/routes/site.php
+++ app/routes/site.php
@@ -9,25 +9,27 @@
 
 $app->get('/',function() use ($app,$blueridge){	
     $app->render('site/home.html');
+    $app->response->headers->set('Content-Type', 'text/html');
 });
-$app->response->headers->set('Content-Type', 'text/html');
 
 $app->get('/pricing/',function() use ($app){
     $app->render('site/pricing.html');
+    $app->response->headers->set('Content-Type', 'text/html');
 });
-$app->response->headers->set('Content-Type', 'text/html');
 
 $app->get('/preview/',function() use ($app){
     $app->render('site/preview.html');
+    $app->response->headers->set('Content-Type', 'text/html');
 });
-$app->response->headers->set('Content-Type', 'text/html');
+
 
 $app->get('/privacy/',function() use ($app){
     $app->render('site/privacy.html');
+    $app->response->headers->set('Content-Type', 'text/html');
 });
 
 $app->get('/about/',function() use ($app){
     $app->render('site/about.html');
+    $app->response->headers->set('Content-Type', 'text/html');
 });
 
-$app->response->headers->set('Content-Type', 'text/html');
