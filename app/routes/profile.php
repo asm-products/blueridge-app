62912e4e20bd417730cbe02f993e4ec2db71d9ee
Moses Ngone <moses@mospired.com>
2014-01-10 06:12:09 -0500

    update fresque
    - added mongo handler to log
    - removed customer subscriptions from que
    - changed variables that are sent to que
    - added mail templates

--- app/routes/profile.php
+++ app/routes/profile.php
@@ -6,12 +6,13 @@
  * @author Moses Ngone <moses@ninelabs.com>
  * @since 0.1.0
  */
+use Blueridge\Utilities\Teller;
 
 $app->get('/app/profile/',function () use ($app,$blueridge) {
 
     $userQr= $blueridge['documentManager']->getRepository('\Blueridge\Documents\User');
-    $user = $userQr->findOneByIdentifier($blueridge['authenticationService']->getIdentity());
-    
+    $user = $userQr->findOneByIdentifier($blueridge['authenticationService']->getIdentity());    
+
     $view = [
     'user' =>$user->toArray(),
     'subscriber'=>$blueridge['configs']['services']['subscriber'],
