7a3da74452dc121bc96c3ad46318882184c2e0a4
Moses Ngone <moses@mospired.com>
2014-02-04 02:45:10 -0500

    sync user todos

--- cli/workers/sync-user-todos.php
+++ cli/workers/sync-user-todos.php
@@ -8,60 +8,13 @@
 require realpath(dirname(__FILE__).'/../init.php');
 
 use Blueridge\Documents\User;
-use Blueridge\Documents\Todo;
-use Blueridge\Providers\Basecamp;
 use Blueridge\Application;
 
 
+$blueridge = new Application();
+$userQr= $blueridge['documentManager']->getRepository('\Blueridge\Documents\User');
 
-// get the user id
-$options = getopt("u:");
-if(empty($options['u']))
-{
-
-    try {
-        throw new Exception("Error Processing Request: User Id missing", 1);
-    } catch (Exception $e) {
-        error_log($e->getMessage()."\n");
-    }
-    exit(1);    
+$users = $userQr->fetchAll();
+foreach ($users as $user) {
+    Resque::enqueue('sync', 'Blueridge\Jobs\FetchBasecampTodos', ['userid'=>$user->id]);
 }
-
-$blueridge = new Blueridge();
-
-
-$id = $options['u'];
-$user = $blueridge['documentManager']->find('\Blueridge\Documents\User', $id);
-$todoQr= $blueridge['documentManager']->getRepository('\Blueridge\Documents\Todo');
-
-$basecampClient = new Basecamp($blueridge);
-$todos = $basecampClient->getTodos($user);
-
-// $todoIds = array();
-
-foreach($todos as $item)
-{
-    $item['todoId']=$item['rel']['project']['account']['product'].'_'.$item['id'];
-    unset($item['id']);
-    $item['source'] = $basecampClient->getTodo($user,$item['url']);
-
-    // check for existing todo and update
-    $todo = $todoQr->findOneByTodoId($item['todoId']);
-
-    if(empty($todo))
-    {
-        $todo = new Todo();
-    }
-
-    $item  = $todo->polish($item);
-    $todo->setProperties($item);        
-    $blueridge['documentManager']->persist($todo);
-                            
-}
-$blueridge['documentManager']->flush();
-
-// log this job
-var_dump(count($todos));
-
-
-// var_dump($todoUrls);
